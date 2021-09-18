a1dfc34430f32680a0eb20e13e96dba000e1ed88
hasmurat <muratkangal0101@gmail.com>
2021-09-18 18:00:04 +0300

    my first commit

--- src/test/java/com/moneygaming/test/Test1.java
+++ src/test/java/com/moneygaming/test/Test1.java
@@ -1,7 +1,27 @@
 package com.moneygaming.test;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class Test1 {
 
+    @BeforeMethod
+    public void setUp(){
 
+    }
+
+
+    @Test
+    public void task1(){
+
+    }
+
+
+
+    @AfterMethod
+    public void tearDown(){
+
+    }
 
 }
